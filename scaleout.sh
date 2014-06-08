271fd4ef24c8a08dd6cdf3d9e1fe7d5a032796af
mukai_masaki <mukai_masaki@a13178-dev02.amb-stg-281.a4c.jp>
2014-06-09 01:14:47 +0900

    initial commit

---
+++ scaleout.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+PID=/home/share/mukai_masaki/scaleup.pid
+INSTANCE=0
+ID=zabbix-httpd
+CPU=0.5
+IP=10.34.48.194
+
+echo aiueo >> /home/share/mukai_masaki/a.txt
+whoami >> /home/share/mukai_masaki/a.txt
+if [ -f $PID ]; then
+  INSTANCES=`http GET http://localhost:8080/v2/apps | jq '.apps[]' | jq "select(.id == \"${ID}\") .instances"`
+  INSTANCES=`expr $INSTANCES + 1`
+  echo $INSTANCES >> /home/share/mukai_masaki/a.txt
+  http --verbose --ignore-stdin PUT http://$IP:8080/v2/apps/${ID} instances=$INSTANCES 2>> /home/share/mukai_masaki/a.txt
+else
+  sudo curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" $IP:8080/v2/apps -d "{\"id\": \"${ID}\", \"cmd\": \"marshi/zabbix-httpd\", \"instances\": 1, \"mem\": 128, \"ports\":[22, 80, 10050], \"cpus\": ${CPU}, \"executor\": \"/var/lib/mesos/executors/docker\"}"
+  echo $$ > $PID
+fi
+echo $? >> /home/share/mukai_masaki/a.txt
+echo =============================== >> /home/share/mukai_masaki/a.txt
