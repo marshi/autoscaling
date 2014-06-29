f402f1e36995a568dab54677bd8ebfcf98a6b496
mukai_masaki <mukai_masaki@a13178-dev02.amb-stg-281.a4c.jp>
2014-06-29 17:35:46 +0900

    change

--- cpu.sh
+++ cpu.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# usage: ./cpu.sh 
+# usage: cpu.sh [-d] [container]
+#
+# cpu使用率を出力.
+#
 
 LXC_CPU_DIR=/cgroup/cpu/cpuacct/lxc
 USAGE="usage: cpu.sh [-d] [container]"
@@ -10,22 +13,26 @@ cpu(){
   dir=$LXC_CPU_DIR/$container
   cpu_file=$dir/cpuacct.stat
   time_file=$dir/cpuacct.usage
+  if [ ! -e $cpu_file -o ! -e $time_file ]; then
+    return 
+  fi
 
   #経過時間の差分を計算
-  cur_time=`cat $time_file`
-  egrep [0-9]+ /tmp/$container.usage > /dev/null &2>1
+  cur_time=`cat $time_file` 
+  egrep [0-9]+ /tmp/$container.usage > /dev/null 
   if [ $? -eq 0 ]; then
-    prev_time=`cat /tmp/$container.usage`
+    prev_time=`cat /tmp/$container.usage` > /dev/null 
   else
     prev_time=0
   fi 
   diff_time=`expr $cur_time - $prev_time | xargs -i echo "scale=5;{} / 1000 / 1000 / 1000" | bc`
 
   #CPU使用時間の差分を計算
-  cur_cpu_time=`grep user $cpu_file | cut -d " " -f 2`
-  egrep [0-9]+ /tmp/$container.stat >/dev/null &2>1
+  user_cpu=`grep user $cpu_file`
+  cur_cpu_time=`echo $user_cpu | cut -d " " -f 2`
+  egrep [0-9]+ /tmp/$container.stat > /dev/null 
   if [ $? -eq 0 ]; then
-    prev_cpu_time=`cat /tmp/$container.stat`
+    prev_cpu_time=`cat /tmp/$container.stat` > /dev/null
   else
     prev_cpu_time=0
   fi 
@@ -33,7 +40,8 @@ cpu(){
   if [ -n "$DEBUG_FLG" ]; then
     echo $container
   fi
-  echo "scale=5; $diff_cpu_time / $diff_time * 100" | bc
+  res_cpu=`echo "scale=5; $diff_cpu_time / $diff_time * 100" | bc`
+  echo $res_cpu
   #現在経過時間の格納
   echo $cur_time > /tmp/$container.usage
   echo $cur_cpu_time > /tmp/$container.stat
--- send_zabbix_cpu.sh
+++ send_zabbix_cpu.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# ./send_zabbix_cpu.sh
+#
+# zabbixサーバから起動IPに属するホスト一覧を取得し、そのホスト上で起動しているdockerのcpu使用率をsend_zabbix APIを使ってzabbixサーバに送る.
+#
+
 SERVER_IP=10.34.48.194
 IP=`ifconfig eth0|grep inet|awk '{print $2}'|cut -d: -f2`
 
@@ -21,4 +26,8 @@ JSON="{
 }"
 
 HOSTS=`curl -X GET -H "Content-Type:application/json-rpc" -d "${JSON}" http://${SERVER_IP}/zabbix/api_jsonrpc.php |  jq ".result[].host"`
-echo $HOSTS
+for host in $HOSTS; do
+  host=`echo $host | sed 's/\"//g'`
+  cpu=`sh cpu.sh $host`
+  echo $cpu
+done
