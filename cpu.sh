d3a768ad6c014aeffd1778e398cd57bc869bd485
mukai_masaki <mukai_masaki@a13178-dev02.amb-stg-281.a4c.jp>
2014-06-29 13:28:02 +0900

    modify cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -4,14 +4,15 @@
 
 LXC_CPU_DIR=/cgroup/cpu/cpuacct/lxc
 
-for dir in `find $LXC_CPU_DIR/* -type d`; do
-  container=`basename $dir`
+cpu(){
+  container=$1
+  dir=$LXC_CPU_DIR/$container
   cpu_file=$dir/cpuacct.stat
   time_file=$dir/cpuacct.usage
 
   #経過時間の差分を計算
   cur_time=`cat $time_file`
-  egrep [0-9]+ /tmp/$container.usage $2>1 > /dev/null
+  egrep [0-9]+ /tmp/$container.usage > /dev/null &2>1
   if [ $? -eq 0 ]; then
     prev_time=`cat /tmp/$container.usage`
   else
@@ -21,7 +22,7 @@ for dir in `find $LXC_CPU_DIR/* -type d`; do
 
   #CPU使用時間の差分を計算
   cur_cpu_time=`grep user $cpu_file | cut -d " " -f 2`
-  egrep [0-9]+ /tmp/$container.stat $2>1 > /dev/null
+  egrep [0-9]+ /tmp/$container.stat >/dev/null &2>1 
   if [ $? -eq 0 ]; then
     prev_cpu_time=`cat /tmp/$container.stat`
   else
@@ -34,4 +35,27 @@ for dir in `find $LXC_CPU_DIR/* -type d`; do
   #現在経過時間の格納
   echo $cur_time > /tmp/$container.usage
   echo $cur_cpu_time > /tmp/$container.stat
+}
+
+all(){
+for dir in `find $LXC_CPU_DIR/* -type d`; do
+  container=`basename $dir`
+  cpu $container
 done
+}
+
+if [ $# -eq 0 ]; then
+  all
+  exit 0
+fi
+
+if [ $# -eq 1 ];then
+  container=$1
+  cpu $container
+  exit 0
+else
+  echo usage: cpu.sh container
+  exit 1
+fi
+
+
