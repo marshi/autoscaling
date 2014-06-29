a1204d28b41fb97c4030e0246d1a7a798b6c915f
mukai_masaki <mukai_masaki@a13178-dev02.amb-stg-281.a4c.jp>
2014-06-29 22:30:14 +0900

    scalein instance check

--- scalein.sh
+++ scalein.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-PID=/home/share/mukai_masaki/scaleup.pid
 INSTANCE=0
 ID=zabbix-httpd
 CPU=0.5
 IP=10.34.48.194
 
-whoami >> /home/share/mukai_masaki/a.txt
-INSTANCES=`http GET http://localhost:8080/v2/apps | jq '.apps[]' | jq "select(.id == \"${ID}\") .instances"`
+INSTANCES=`http GET http://${IP}:8080/v2/apps | jq '.apps[]' | jq "select(.id == \"${ID}\") .instances"`
 echo $INSTANCES
+if [ -z $INSTANCES ]; then
+  echo instance = $INSTANCES
+  exit 1
+fi
 if [ $INSTANCES -le 0 ]; then
-  return
+  exit 1
 fi
 INSTANCES=`expr $INSTANCES - 1`
-echo $INSTANCES >> /home/share/mukai_masaki/a.txt
-http --verbose --ignore-stdin PUT http://$IP:8080/v2/apps/${ID} instances=$INSTANCES 2>> /home/share/mukai_masaki/a.txt
+echo $INSTANCES 
+http --verbose --ignore-stdin PUT http://$IP:8080/v2/apps/${ID} instances=$INSTANCES
