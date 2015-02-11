92f7e80addc207da08646d5c6e1d51315b4f4666
marshi <mashi3648@gmail.com>
2015-02-12 02:01:30 +0900

    add libvirt mode

--- cpu.sh
+++ cpu.sh
@@ -5,12 +5,17 @@
 # cpu使用率を出力.
 #
 
-LXC_CPU_DIR=/cgroup/cpu/cpuacct/lxc
-USAGE="usage: cpu.sh [-d] [container]"
+USAGE="usage: cpu.sh [-d] [-c \"docker or libvirt\"] [container] "
+CONTAINER_TYPE="docker"
+DEBUG_FLG=0
 
 cpu(){
-  container=$1
+  container=${1}.libvirt-lxc
   dir=$LXC_CPU_DIR/$container
+  if [ $DEBUG_FLG -eq 1 -a ! -d $dir ]; then
+    echo "container not found; check option -c(CONTAINER TYPE. docker or libvirt)"
+    exit 1;
+  fi
   cpu_file=$dir/cpuacct.stat
   time_file=$dir/cpuacct.usage
   if [ ! -e $cpu_file -o ! -e $time_file ]; then
@@ -25,7 +30,11 @@ cpu(){
   else
     prev_time=0
   fi 
-  diff_time=`expr $cur_time - $prev_time | xargs -i echo "scale=5;{} / 1000 / 1000 / 1000" | bc`
+  if [ $CONTAINER_TYPE = "libvirt" ]; then
+    diff_time=`expr $cur_time - $prev_time | xargs -i echo "scale=5;{} / 1000 / 100" | bc`
+  else 
+    diff_time=`expr $cur_time - $prev_time | xargs -i echo "scale=5;{} / 1000 / 1000 / 1000" | bc`
+  fi
 
   #CPU使用時間の差分を計算
   user_cpu=`grep user $cpu_file`
@@ -40,7 +49,12 @@ cpu(){
   if [ -n "$DEBUG_FLG" ]; then
     echo $container
   fi
-  res_cpu=`echo "scale=5; $diff_cpu_time / $diff_time * 100" | bc`
+  
+  if [ `echo "$diff_time == 0" | bc` -eq 1 ]; then
+    res_cpu=0
+  else
+    res_cpu=`echo "scale=5; $diff_cpu_time / $diff_time * 100" | bc`
+  fi
   echo $res_cpu
   #現在経過時間の格納
   echo $cur_time > /tmp/$container.usage
@@ -54,11 +68,14 @@ for dir in `find $LXC_CPU_DIR/* -type d`; do
 done
 }
 
-while getopts d opt
+while getopts dc: opt
 do
   case ${opt} in
     d)
       DEBUG_FLG=1
+      ;;
+    c)
+      CONTAINER_TYPE=$OPTARG
       ;;
     \?)
       echo $USAGE
@@ -68,6 +85,13 @@ do
 done
 
 shift $((OPTIND - 1))
+
+
+if [ ${CONTAINER_TYPE}x = "libvirt"x ]; then
+  LXC_CPU_DIR=/sys/fs/cgroup/cpuacct/machine
+else
+  LXC_CPU_DIR=/cgroup/cpu/cpuacct/lxc
+fi
 
 if [ $# -eq 0 ]; then
   all
